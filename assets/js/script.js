12757d333b2e1fba4c601058c649f95f2daeb4f1
Alexiou981 <>
2023-12-04 19:38:17 +0000

    feat: add js code for button functionality

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function showChoices() {
+    var x = document.getElementById("gameplay-area");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
