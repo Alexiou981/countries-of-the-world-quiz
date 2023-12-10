5c581a7bc09465672f823591254fd1f60463d88f
Alexiou981 <>
2023-12-10 17:46:31 +0000

    feat: add next flag function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,12 @@ const countryFlag = document.getElementById("random-flag");
 const imgFlags = ["assets/images/flags/algeria.png", "assets/images/flags/argentina.png", "assets/images/flags/australia.png", "assets/images/flags/bahamas.png", "assets/images/flags/bangadesh.png", "assets/images/flags/belgium.png"];
 
 function nextCountryFlag() {
-    const randomImage = Math.floor(Math.random() * imgFlags.length);
-    countryFlag.src = imgFlags[randomImage];
-    localStorage.setItem('flag', randomImage);
-}
+    var nextBtn = document.getElementById("next-flag-btn");
+    nextBtn.addEventListener("click", () => {
+        const nextRandomImage = Math.floor(Math.random() * imgFlags.length);
+        countryFlag.src = imgFlags[nextRandomImage];
+        localStorage.setItem('flag', nextRandomImage);
+    })
+
+};
 
