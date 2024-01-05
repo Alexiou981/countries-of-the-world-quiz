fe3cd260fe865ab000564afcd2220a2f9139da5e
Alexiou981 <>
2024-01-05 21:11:55 +0000

    feat: add code to replace choices depending on flag img

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,32 +9,38 @@ questionCounter = 1;
 
 function startGame() {
     var startBtn = document.getElementById("start-game-btn");
-        /* Ensures that the Start Game button dissappears once clicked */
-        startBtn.style.display = "none";
+    /* Ensures that the Start Game button dissappears once clicked */
+    startBtn.style.display = "none";
 
-        /* Additionally, make those elements show up or dissappear once the button is clicked */
-        document.getElementById("gameplay-area").style.display = "block";
-        document.getElementById("landing-flag").style.display = "none";
-        document.getElementById("next-flag-btn").style.display = "block";
-        document.getElementById("random-flag").style.display = "block";
+    /* Additionally, make those elements show up or dissappear once the button is clicked */
+    document.getElementById("gameplay-area").style.display = "block";
+    document.getElementById("landing-flag").style.display = "none";
+    document.getElementById("next-flag-btn").style.display = "block";
+    document.getElementById("random-flag").style.display = "block";
 
 
-        /*
-          Ideas on how to create a function that will choose 
-          random image from an array were taken from this source:
-          https://openjavascript.info/2022/12/11/random-image-with-javascript/ 
-        */
-        const randomImg = Math.floor(Math.random() * choices.length);
-        countryFlag.src = choices[randomImg].image;
+    /*
+      Ideas on how to create a function that will choose 
+      random image from an array were taken from this source:
+      https://openjavascript.info/2022/12/11/random-image-with-javascript/ 
+    */
+    const randomImg = Math.floor(Math.random() * choices.length);
+    countryFlag.src = choices[randomImg].image;
 
-        /*
-          Ideas on how to ensure the image doesn't get picked 
-          twice were taken from this source: 
-          https://www.w3schools.com/jsref/jsref_splice.asp
-         */
-        choices.splice(randomImg, 1);
-        console.log(randomImg);
-    
+    /*
+      Ideas on how to ensure the image doesn't get picked 
+      twice were taken from this source: 
+      https://www.w3schools.com/jsref/jsref_splice.asp
+     */
+
+    optionBtns.innerHTML = `
+        <button>${choices[randomImg].answers[0].text}</button>
+        <button>${choices[randomImg].answers[1].text}</button>
+        <button>${choices[randomImg].answers[2].text}</button>
+        <button>${choices[randomImg].answers[3].text}</button>`;
+    choices.splice(randomImg, 1);
+    console.log(randomImg);
+
 };
 
 /* 
@@ -71,6 +77,11 @@ function chooseFlag() {
       twice were taken from this source: 
       https://www.w3schools.com/jsref/jsref_splice.asp
     */
+    optionBtns.innerHTML = `
+      <button>${choices[randomImg].answers[0].text}</button>
+      <button>${choices[randomImg].answers[1].text}</button>
+      <button>${choices[randomImg].answers[2].text}</button>
+      <button>${choices[randomImg].answers[3].text}</button>`;
     choices.splice(randomImg, 1);
     console.log(randomImg);
 }
