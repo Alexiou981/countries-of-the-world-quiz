0f9fc94adea4d5fd03b14fbf071579947e706144
Alexiou981 <>
2024-01-06 10:48:01 +0000

    fix: refine code comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,17 @@ function startGame() {
     var startBtn = document.getElementById("start-game-btn");
     /* Ensures that the Start Game button dissappears once clicked */
     startBtn.style.display = "none";
-
     /* Additionally, make those elements show up or dissappear once the button is clicked */
     document.getElementById("gameplay-area").style.display = "block";
     document.getElementById("landing-flag").style.display = "none";
     document.getElementById("next-flag-btn").style.display = "block";
     document.getElementById("random-flag").style.display = "block";
-
-
     /*
-      Ideas on how to create a function that will choose 
-      random image from an array were taken from this source:
+      Ideas on how to choose random image from an array were taken from this source:
       https://openjavascript.info/2022/12/11/random-image-with-javascript/ 
     */
     const randomImg = Math.floor(Math.random() * choices.length);
     countryFlag.src = choices[randomImg].image;
-
-    /*
-      Ideas on how to ensure the image doesn't get picked 
-      twice were taken from this source: 
-      https://www.w3schools.com/jsref/jsref_splice.asp
-     */
 
     optionBtns.innerHTML = `
         <button>${choices[randomImg].answers[0].text}</button>
@@ -44,9 +34,7 @@ function startGame() {
 };
 
 /* 
-  This function increments the number of the question every time
-  the user clicks next and ensures that the next button dissapears 
-  when the number reaches to number 10 which is the final question. 
+   Increments question number diplayed to user when "NEXT" button is clicked. 
 */
 function nextQuestion() {
     chooseFlag()
@@ -65,18 +53,11 @@ function updateDisplay() {
 
 function chooseFlag() {
     /*
-      Ideas on how to create a function that will choose 
-      a random image from an array were taken from this source:
+      Ideas on how to choose random image from an array were taken from this source:
       https://openjavascript.info/2022/12/11/random-image-with-javascript/ 
     */
     const randomImg = Math.floor(Math.random() * choices.length);
     countryFlag.src = choices[randomImg].image;
-
-    /*
-      Ideas on how to ensure the image doesn't get picked 
-      twice were taken from this source: 
-      https://www.w3schools.com/jsref/jsref_splice.asp
-    */
     optionBtns.innerHTML = `
       <button>${choices[randomImg].answers[0].text}</button>
       <button>${choices[randomImg].answers[1].text}</button>
