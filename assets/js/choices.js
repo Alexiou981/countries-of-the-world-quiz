fbb17f82b621b2e9c51b0f6f84d2e14fb965cea5
Alexiou981 <>
2024-01-08 13:36:19 +0000

    fix: rewrite choices array and assaign value to correct option only

--- assets/js/choices.js
+++ assets/js/choices.js
@@ -1,202 +1,204 @@
 const choices = [
     {
       image: "assets/images/flags/algeria.png",
-      answers:[
-        {text: "Greece", correct: false},
-        {text: "Germany", correct: false},
-        {text: "Algeria", correct: true},
-        {text: "Brazil", correct: false}
-      ]
+      answers: [
+        { text: "Greece" },
+        { text: "Germany" },
+        { text: "Algeria" },
+        { text: "Brazil" }
+      ],
+      correct: "Algeria"
     },
     {
       image: "assets/images/flags/argentina.png",
       answers: [
-        {text: "Brazil", correct: false},
-        {text: "Argentina", correct: true},
-        {text: "Colombia", correct: false},
-        {text: "Ecuador", correct: false}
-  
-      ]
+        { text: "Brazil" },
+        { text: "Argentina" },
+        { text: "Colombia" },
+        { text: "Ecuador" }
+      ],
+      correct: "Argentina"
     },
     {
       image: "assets/images/flags/australia.png",
       answers: [
-        {text: "Greece", correct: false},
-        {text: "Australia", correct: true},
-        {text: "Bahamas", correct: false},
-        {text: "Cyprus", correct: false}
-  
-      ]
-    },   
+        { text: "Greece" },
+        { text: "Australia" },
+        { text: "Bahamas" },
+        { text: "Cyprus" }
+      ],
+      correct: "Australia"
+    },
     {
       image: "assets/images/flags/bahamas.png",
       answers: [
-        {text: "Malaysia", correct: false},
-        {text: "Lebanon", correct: false},
-        {text: "Spain", correct: false},
-        {text: "Bahamas", correct: true}
-  
-      ]
+        { text: "Malaysia" },
+        { text: "Lebanon" },
+        { text: "Spain" },
+        { text: "Bahamas" }
+      ],
+      correct: "Bahamas"
     },
     {
       image: "assets/images/flags/bangladesh.png",
       answers: [
-        {text: "Bangladesh", correct: true},
-        {text: "Chile", correct: false},
-        {text: "Germany", correct: false},
-        {text: "India", correct: false}
-  
-      ]
+        { text: "Bangladesh"},
+        { text: "Chile" },
+        { text: "Germany" },
+        { text: "India" }
+      ],
+      correct: "Bangladesh"
     },
-    {
-      image: "assets/images/flags/belgium.png",
-      answers: [
-        {text: "Germany", correct: false},
-        {text: "Belgium", correct: true},
-        {text: "Indonesia", correct: false},
-        {text: "Ireland", correct: false}
+  {
+    image: "assets/images/flags/belgium.png",
+    answers: [
+      {text: "Germany"},
+      {text: "Belgium"},
+      {text: "Indonesia"},
+      {text: "Ireland"}
+    ],
+    correct: "Belgium"
+  },
+  {
+    image: "assets/images/flags/belize.png",
+    answers: [
+      {text: "Iran"},
+      {text: "Ireland"},
+      {text: "Belize"},
+      {text: "Malaysia"}
+    ],
+    correct: "Belize"
+  },
+  {
+    image: "assets/images/flags/bolivia.png",
+    answers: [
+      {text: "Lybia"},
+      {text: "Singapore"},
+      {text: "Saudi-Arabia"},
+      {text: "Bolivia"}
+    ],
+    correct: "Bolivia"
+  },
+  {
+    image: "assets/images/flags/brazil.png",
+    answers: [
+      {text: "Kenya"},
+      {text: "Brazil"},
+      {text: "New-Zealand"},
+      {text: "Spain"}
+    ],
+    correct: "Brazil"
+  },
+  {
+    image: "assets/images/flags/bulgaria.png",
+    answers: [
+      {text: "Bulgaria"},
+      {text: "Finland"},
+      {text: "Cameroon"},
+      {text: "Cambodia"}
+    ],
+    correct: "Bulgaria"
+  },
+  {
+    image: "assets/images/flags/cambodia.png",
+    answers: [
+      {text: "Greece"},
+      {text: "Germany"},
+      {text: "Cambodia"},
+      {text: "Italy"}
+    ],
+    correct: "Cambodia"
+  },
+  {
+    image: "assets/images/flags/cameroon.png",
+    answers: [
+      {text: "India"},
+      {text: "Cameroon"},
+      {text: "Iran"},
+      {text: "Lebanon"}
+    ],
+    correct: "Cameroon"
+  },
+  {
+    image: "assets/images/flags/chile.png",
+    answers: [
+      {text: "Algeria"},
+      {text: "Sri-Lanka"},
+      {text: "Lybia"},
+      {text: "Chile"}
+    ],
+    correct: "Chile"
+  },
+  {
+    image: "assets/images/flags/colombia.png",
+    answers: [
+      {text: "Japan"},
+      {text: "Colombia"},
+      {text: "Australia"},
+      {text: "Bahamas"}
+    ],
+    correct: "Colombia"
+  },
+  {
+    image: "assets/images/flags/costa-rica.png",
+    answers: [
+      {text: "Singapore"},
+      {text: "Belgium"},
+      {text: "Costa-Rica"},
+      {text: "Bulgaria"}
+    ],
+    correct: "Costa-Rica"
+  },
+  {
+    image: "assets/images/flags/cuba.png",
+    answers: [
+      {text: "Cuba"},
+      {text: "Cambodia"},
+      {text: "Spain"},
+      {text: "Italy"}
+    ],
+    correct: "Cuba"
+  },
+  {
+    image: "assets/images/flags/finland.png",
+    answers: [
+      {text: "Belize"},
+      {text: "Finland"},
+      {text: "Bangladesh"},
+      {text: "Cameroon"}
+    ],
+    correct: "Finland"
+  },
+  {
+    image: "assets/images/flags/germany.png",
+    answers: [
+      {text: "Italy"},
+      {text: "Germany"},
+      {text: "Greece"},
+      {text: "Ireland"}
+    ],
+    correct: "Germany"
+  },
+  {
+    image: "assets/images/flags/greece.png",
+    answers: [
+      {text: "Chile"},
+      {text: "Colombia"},
+      {text: "Cuba"},
+      {text: "Greece"}
+    ],
+    correct: "Greece"
+  },
+  {
+    image: "assets/images/flags/india.png",
+    answers: [
+      {text: "Bangladesh"},
+      {text: "India"},
+      {text: "Sri-Lanka"},
+      {text: "Costa-Rica"}
+    ],
+    correct: "India"
+  }
+  ]
+   console.log(choices)
   
-      ]
-    },
-    {
-      image: "assets/images/flags/belize.png",
-      answers: [
-        {text: "Iran", correct: false},
-        {text: "Ireland", correct: false},
-        {text: "Belize", correct: true},
-        {text: "Malaysia", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/bolivia.png",
-      answers: [
-        {text: "Lybia", correct: false},
-        {text: "Singapore", correct: false},
-        {text: "Saudi-Arabia", correct: false},
-        {text: "Bolivia", correct: true}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/brazil.png",
-      answers: [
-        {text: "Kenya", correct: false},
-        {text: "Brazil", correct: true},
-        {text: "New-Zealand", correct: false},
-        {text: "Spain" , correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/bulgaria.png",
-      answers: [
-        {text: "Bulgaria", correct: true},
-        {text: "Finland", correct: false},
-        {text: "Cameroon", correct: false},
-        {text: "Cambodia", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/cambodia.png",
-      answers: [
-        {text: "Greece", correct: false},
-        {text: "Germany", correct: false},
-        {text: "Cambodia", correct: true},
-        {text: "Italy", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/cameroon.png",
-      answers: [
-        {text: "India", correct: false},
-        {text: "Cameroon", correct: true},
-        {text: "Iran", correct: false},
-        {text: "Lebanon", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/chile.png",
-      answers: [
-        {text: "Algeria", correct: false},
-        {text: "Sri-Lanka", correct: false},
-        {text: "Lybia", correct: false},
-        {text: "Chile", correct: true}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/colombia.png",
-      answers: [
-        {text: "Japan", correct: false},
-        {text: "Colombia", correct: true},
-        {text: "Australia", correct: false},
-        {text: "Bahamas", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/costa-rica.png",
-      answers: [
-        {text: "Singapore", correct: false},
-        {text: "Belgium", correct: false},
-        {text: "Costa-Rica", correct: true},
-        {text: "Bulgaria", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/cuba.png",
-      answers: [
-        {text: "Cuba", correct: true},
-        {text: "Cambodia", correct: false},
-        {text: "Spain", correct: false},
-        {text: "Italy", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/finland.png",
-      answers: [
-        {text: "Belize", correct: false},
-        {text: "Finland", correct: true},
-        {text: "Bangladesh", correct: false},
-        {text: "Cameroon", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/germany.png",
-      answers: [
-        {text: "Italy", correct: false},
-        {text: "Germany", correct: true},
-        {text: "Greece", correct: false},
-        {text: "Ireland", correct: false}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/greece.png",
-      answers: [
-        {text: "Chile", correct: false},
-        {text: "Colombia", correct: false},
-        {text: "Cuba", correct: false},
-        {text: "Greece", correct: true}
-  
-      ]
-    },
-    {
-      image: "assets/images/flags/india.png",
-      answers: [
-        {text: "Balngladesh", correct: false},
-        {text: "India", correct: true},
-        {text: "Sri-Lanka", correct: false},
-        {text: "Costa-Rica", correct: false}
-      ]
-    }
-  ];
-  
-  console.log(choices);
