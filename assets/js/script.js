57de3801a8903f75777396c54d6c6264e1c9b2d5
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-01-14 11:32:27 +0000

    chore: tidy-up the code so it passes through jshint without errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 // Assign variables and get elements from the DOM
 // Hides rematch button and exit message and button
 
-let countryFlag = document.getElementById("random-flag")
-const optionBtns = document.getElementById("multiple-choices")
-const nextBtn = document.getElementById("next-flag-btn")
-const rematchBtn = document.getElementById("rematch")
-const result = document.getElementById("result")
-const exitMsg = document.getElementById("exit-message")
-const exitBtn = document.getElementById("exit")
+let countryFlag = document.getElementById("random-flag");
+const optionBtns = document.getElementById("multiple-choices");
+const rematchBtn = document.getElementById("rematch");
+const result = document.getElementById("result");
+const exitMsg = document.getElementById("exit-message");
+const exitBtn = document.getElementById("exit");
 var userAnswer;
-var questionCounter = 1
-var correctAnswers = 0
-var incorrectAnswers = 0
-exitMsg.style.display = "none"
-rematchBtn.style.display = "none"
-exitBtn.style.display = "none"
+var questionCounter = 1;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+exitMsg.style.display = "none";
+rematchBtn.style.display = "none";
+exitBtn.style.display = "none";
+
 
 
 /**
@@ -39,17 +39,17 @@ function startGame() {
   randomImg = Math.floor(Math.random() * choices.length);
   countryFlag.src = choices[randomImg].image;
 
-  option1 = document.getElementById("option1")
-  option2 = document.getElementById("option2")
-  option3 = document.getElementById("option3")
-  option4 = document.getElementById("option4")
-  option1.innerHTML = choices[randomImg].answers[0].text
-  option2.innerHTML = choices[randomImg].answers[1].text
-  option3.innerHTML = choices[randomImg].answers[2].text
-  option4.innerHTML = choices[randomImg].answers[3].text
+  option1 = document.getElementById("option1");
+  option2 = document.getElementById("option2");
+  option3 = document.getElementById("option3");
+  option4 = document.getElementById("option4");
+  option1.innerHTML = choices[randomImg].answers[0].text;
+  option2.innerHTML = choices[randomImg].answers[1].text;
+  option3.innerHTML = choices[randomImg].answers[2].text;
+  option4.innerHTML = choices[randomImg].answers[3].text;
 
-  whiteOptionBtns()
-};
+  whiteOptionBtns();
+}
 
 /** 
    This function is available to the user upon choosing one of the options,
@@ -58,9 +58,9 @@ function startGame() {
 function nextQuestion() {
   document.getElementById("next-flag-btn").style.display = "none";
   document.getElementById("question").style.display = "block";
-  chooseFlag()
-  enableOptionBtns()
-  whiteOptionBtns()
+  chooseFlag();
+  enableOptionBtns();
+  whiteOptionBtns();
   questionCounter++;
   if (questionCounter < 10) {
     updateDisplay();
@@ -68,7 +68,7 @@ function nextQuestion() {
     updateDisplay();
     document.getElementById("next-flag-btn").style.display = "none";
   }
-};
+}
 
 /**
  * Updates the number of currect question out of ten
@@ -90,10 +90,10 @@ function chooseFlag() {
   randomImg = Math.floor(Math.random() * choices.length);
   countryFlag.src = choices[randomImg].image;
 
-  option1.innerHTML = choices[randomImg].answers[0].text
-  option2.innerHTML = choices[randomImg].answers[1].text
-  option3.innerHTML = choices[randomImg].answers[2].text
-  option4.innerHTML = choices[randomImg].answers[3].text
+  option1.innerHTML = choices[randomImg].answers[0].text;
+  option2.innerHTML = choices[randomImg].answers[1].text;
+  option3.innerHTML = choices[randomImg].answers[2].text;
+  option4.innerHTML = choices[randomImg].answers[3].text;
 }
 
 /**
@@ -108,19 +108,19 @@ function selectAnswer() {
   https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified 
   */
   optionBtns.addEventListener("click", function (opt) {
-    userAnswer = opt.target
-  })
+    userAnswer = opt.target;
+  });
   const disableOptionBnts = () => {
     option1.disabled = true,
     option2.disabled = true,
     option3.disabled = true,
-    option4.disabled = true
-  }
-  optionBtns.addEventListener("click", disableOptionBnts)
+    option4.disabled = true;
+  };
+  optionBtns.addEventListener("click", disableOptionBnts);
 }
 
 
-selectAnswer()
+selectAnswer();
 
 /**
  * Checks the answer selected by the user 
@@ -128,53 +128,52 @@ selectAnswer()
  */
 function checkAnswer() {
   optionBtns.addEventListener("click", function () {
-    let correctAnswer = choices[randomImg].correct
+    let correctAnswer = choices[randomImg].correct;
     if (correctAnswer === userAnswer.innerText) {
-      userAnswer.style.backgroundColor = "green"
-      incrementCorrectScore()
+      userAnswer.style.backgroundColor = "green";
+      incrementCorrectScore();
     } else {
-      userAnswer.style.backgroundColor = "red"
-      incrementIncorrectScore()
+      userAnswer.style.backgroundColor = "red";
+      incrementIncorrectScore();
     }
     // Removes selected image from choices array
-    choices.splice(randomImg, 1)
+    choices.splice(randomImg, 1);
     document.getElementById("next-flag-btn").style.display = "block";
     document.getElementById("question").style.display = "none";
     // Show final result depending on the amount of correct/incorrect answers
     if (questionCounter === 10) {
       document.getElementById("next-flag-btn").style.display = "none";
       if (correctAnswers > incorrectAnswers) {
-        result.innerHTML = "<h1>You Win!</h1>"
-        result.style.textAlign = "center"
+        result.innerHTML = "<h1>You Win!</h1>";
+        result.style.textAlign = "center";
       } else if (correctAnswers === incorrectAnswers) {
-        result.innerHTML = "<h1>It's a draw. <br> No winner this time.</h1>"
-        result.style.textAlign = "center"
+        result.innerHTML = "<h1>It's a draw. <br> No winner this time.</h1>";
+        result.style.textAlign = "center";
       } else {
-        result.innerHTML = "<h1>You Lose!</h1>"
-        result.style.textAlign = "center"
+        result.innerHTML = "<h1>You Lose!</h1>";
+        result.style.textAlign = "center";
       }
       // Hides elements after answering the last flag question
-      document.getElementById("random-flag").style.display = "none"
-      document.getElementById("question").style.display = "none"
-      document.getElementById("counter-area").style.display = "none"
-      optionBtns.style.display = "none"
-      rematchBtn.style.display = "block"
-      exitBtn.style.display = "block"
+      document.getElementById("random-flag").style.display = "none";
+      document.getElementById("question").style.display = "none";
+      document.getElementById("counter-area").style.display = "none";
+      optionBtns.style.display = "none";
+      rematchBtn.style.display = "block";
+      exitBtn.style.display = "block";
       /* Ideas on how to remove an element from DOM were raken from:
       https://developer.mozilla.org/en-US/docs/Web/API/Element/remove
       */
       // Removes element from DOM before announcing final result
-      document.getElementById("flag-container").remove()
+      document.getElementById("flag-container").remove();
     }
-  })
+  });
 }
-checkAnswer()
+checkAnswer();
 
 /**
  * Increments correct score
  */
 function incrementCorrectScore() {
-  let previousScore = (document.getElementById("correct-answers").innerText);
   (document.getElementById("correct-answers").innerText) = ++correctAnswers;
 }
 
@@ -182,7 +181,6 @@ function incrementCorrectScore() {
  * Increments incorrect score
  */
 function incrementIncorrectScore() {
-  let previousScore = (document.getElementById("wrong-answers").innerText);
   (document.getElementById("wrong-answers").innerText) = ++incorrectAnswers;
 }
 
@@ -193,7 +191,7 @@ function enableOptionBtns() {
   option1.disabled = false,
   option2.disabled = false,
   option3.disabled = false,
-  option4.disabled = false
+  option4.disabled = false;
 }
 
 /**
@@ -203,7 +201,7 @@ function whiteOptionBtns() {
   option1.style.backgroundColor = "white",
   option2.style.backgroundColor = "white",
   option3.style.backgroundColor = "white",
-  option4.style.backgroundColor = "white"
+  option4.style.backgroundColor = "white";
 }
 
 /**
@@ -215,19 +213,19 @@ function restart() {
   https://www.w3schools.com/jsref/met_loc_reload.asp
   */
 
-  window.location.reload()
+  window.location.reload();
 }
 
 /**
  * Hides elemnts of gameplay area and shows thank you message
  */
 function exit() {
-  exitMsg.style.display = "block"
-  document.getElementById("heading").style.display = "block"
-  document.getElementById("question").style.display = "none"
-  document.getElementById("rematch").style.display = "none"
-  document.getElementById("exit").style.display = "none"
-  document.getElementById("main-game-area").style.display = "none"
-  document.getElementById("counter-area").style.display = "none"
-  document.getElementById("player-score-area").style.display = "none"
+  exitMsg.style.display = "block";
+  document.getElementById("heading").style.display = "block";
+  document.getElementById("question").style.display = "none";
+  document.getElementById("rematch").style.display = "none";
+  document.getElementById("exit").style.display = "none";
+  document.getElementById("main-game-area").style.display = "none";
+  document.getElementById("counter-area").style.display = "none";
+  document.getElementById("player-score-area").style.display = "none";
 }
